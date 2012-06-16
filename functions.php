4657854ed54c851b964ff5a5130492fc6debc65f
Sam King <sdking02@gmail.com>
2012-06-16 18:41:28 +0100

    parse_args function added to functions file; format_mins removed

--- functions.php
+++ functions.php
@@ -5,9 +5,10 @@ function error($msg)
 	exit('<span class="error">' . $msg . '</span>');
 }
 
-function format_mins($mins)
+function parse_args($args)
 {
-	$hours = floor($mins/60);
-	$mins = $mins%60;
-	return $hours . ':' . ($mins < 10? '0' . $mins : $mins);
+	if (is_array($args[0]) || count($args) == 1)
+		return $args[0];
+	else
+		return $args;
 }
