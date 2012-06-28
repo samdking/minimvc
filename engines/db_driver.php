37b56aa856c5ac4b8c53e6c7ff57a7bf644d12a8
Sam King <sdking02@gmail.com>
2012-06-28 22:45:07 +0100

    Make DB_driver class into an abstract singleton

--- engines/db_driver.php
+++ engines/db_driver.php
@@ -1,13 +1,25 @@
 <?php
 
-class DB_driver
+abstract class DB_driver
 {
+	protected static $inst;
+
+	abstract function connect();
+
 	static function get($type)
 	{
 		$class_name = $type . '_db_driver';
 		if (!class_exists($class_name))
 			include dirname(__FILE__) . '/drivers/' . $type . '.php';
+		return $class_name::inst();
+	}
 
-		return new $class_name;
+	static function inst()
+	{
+		if (is_null(static::$inst)) {
+			static::$inst = new static;
+			static::$inst->connect();
+		}
+		return static::$inst;
 	}
 }
