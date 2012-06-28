1de03b74641193a767bf9f032e941789e5641728
Sam King <sdking02@gmail.com>
2012-06-28 22:45:38 +0100

    Make Engine class into an abstract singleton

--- engine.php
+++ engine.php
@@ -1,22 +1,16 @@
 <?php
 
-class Engine
+abstract class Engine
 {
-	function __construct()
-	{
-		$this->init();
-	}
-
-	function init()
-	{
-
-	}
-
 	static function get($type)
 	{
 		$class_name = $type . 'Engine';
 		if (!class_exists($class_name))
 			include dirname(__FILE__) . '/engines/' . $class_name . '.php';
-		return new $class_name;
+		$obj = new $class_name;
+		$obj->init();
+		return $obj;
 	}
+
+	abstract function init();
 }
